993214a071f1186f5b59ebdacd64167790a2a74a
Aditya Eka Maulana <adityaemaulana@gmail.com>
2017-11-11 20:59:10 +0700

    Adds another method

--- PemesananTiketKereta/src/pemesanantiketkereta/Tiket.java
+++ PemesananTiketKereta/src/pemesanantiketkereta/Tiket.java
@@ -1,25 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pemesanantiketkereta;
 
-/**
- *
- * @author AdityaEM
- */
-public class Tiket {
+public abstract class  Tiket {
     private String jamBerangkat;
+    private String jamSampai;
     private String tglBerangkat;
     private String tujuan;
     private String asal;
+    private Kereta kereta;
+    private int id;
+    private int harga;  // Eksekutif = harga + 30% harga, Bisnis = harga + 10% harga, Ekonomi = harga
 
-    public Tiket(String jamBerangkat, String tglBerangkat, String tujuan, String asal) {
+    public Tiket(int id, Kereta kereta, String jamBerangkat, String jamSampai, String tglBerangkat, String asal, String tujuan, int harga) {
+        this.id = id;
+        this.kereta = kereta;
+        this.kereta.tambahJadwal(jamBerangkat, jamSampai);       
         this.jamBerangkat = jamBerangkat;
+        this.jamSampai = jamSampai;
         this.tglBerangkat = tglBerangkat;
         this.tujuan = tujuan;
         this.asal = asal;
+        this.harga = this.setHarga(harga);
+    }
+
+    public int getHarga() {
+        return harga;
+    }   
+    
+    protected int setHarga(int harga) {
+        this.harga = harga;
+        if("Eksekutif".equals(kereta.getKelas())) return harga += (0.3*harga);
+        else if("Bisnis".equals(kereta.getKelas())) return harga += (0.1*harga);
+        else if("Ekonomi".equals(kereta.getKelas())) return harga += harga;
+        else return 0;
+    }
+    
+    public Kereta getKereta(){
+        return kereta;
+    }   
+
+    public String getJamBerangkat() {
+        return jamBerangkat;
+    }
+
+    public String getJamSampai() {
+        return jamSampai;
+    }
+
+    public String getTglBerangkat() {
+        return tglBerangkat;
+    }
+
+    public String getTujuan() {
+        return tujuan;
+    }
+
+    public String getAsal() {
+        return asal;
+    }
+
+    public int getId() {
+        return id;
     }
     
     
