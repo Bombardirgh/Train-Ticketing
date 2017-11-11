f948287522a625a165280d10716920465fd6f0a2
Aditya Eka Maulana <adityaemaulana@gmail.com>
2017-11-11 20:56:48 +0700

    Adds another method

--- PemesananTiketKereta/src/pemesanantiketkereta/Kereta.java
+++ PemesananTiketKereta/src/pemesanantiketkereta/Kereta.java
@@ -1,39 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pemesanantiketkereta;
 
-/**
- *
- * @author AdityaEM
- */
 public class Kereta {
     private String nama;
-    private String noKursi;
-    private String kelas;
-    private Posisi posisi;
+    private int jalur;
+    private String asal;
+    private String tujuan;
+    private String kelas;  //Kelas kereta (Eksekutif, Bisnis, Ekonomi)
     private Jadwal[] jadwal; 
     private int i=0;
  
-    public Kereta(String nama, String noKursi, String kelas) {
-        this.jadwal = jadwal;
+    public Kereta(String nama,int jalur, String asal,String tujuan, String kelas) {
+        this.jalur = jalur;
         this.nama = nama;
-        this.noKursi = noKursi;
+        this.asal = asal;
         this.kelas = kelas;
     }
     
-    public void tambahJadwal(String jam){
-        Jadwal j = new Jadwal(jam);
+    public void tambahJadwal(String jB, String  jS){     
+        Jadwal j = new Jadwal(jB, jS);
         jadwal[i] = j;
         i++;
     }
 
-    public void pindahPosisi(String namaStasiun, int gerbong, int jalur){
-        this.posisi.setNamaStasiun(namaStasiun);
-        this.posisi.setGerbong(gerbong);
-        this.posisi.setJalur(jalur);
+    public void pindahPosisi(String asal, String tujuan, String tujuanBaru){
+        this.asal = tujuan;
+        this.asal = tujuanBaru;
+    } 
+    
+    public Jadwal[] getJadwal(){
+        return this.jadwal;
     }
+    
+    public Jadwal getJadwalByIdx (int idx){
+        return this.jadwal[idx];
+    }
+    
+    public Jadwal getJadwalByJam(String jam){
+         int i = 0;
+         while((i < this.jadwal.length)&& (!jadwal[i].getJamBerangkat().equals(jam))){
+             i++;
+         }
+         Jadwal j = jadwal[i];
+         if(j != null) return j;
+         else{
+             System.out.println("Jadwal tidak ditemukan");
+             return j;
+         }
+    }
+    
+    
+    public String getKelas() {
+        return kelas;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAsal() {
+        return asal;
+    }
+
+    public String getTujuan() {
+        return tujuan;
+    }
+
+    public int getJalur() {
+        return jalur;
+    }
+    
     
 }
