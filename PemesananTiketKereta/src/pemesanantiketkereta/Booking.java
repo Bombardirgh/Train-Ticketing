13cda191458fbbdf315edbd1954a34f680029c0e
Aditya Eka Maulana <adityaemaulana@gmail.com>
2017-11-11 20:55:46 +0700

    Adds another method

--- PemesananTiketKereta/src/pemesanantiketkereta/Booking.java
+++ PemesananTiketKereta/src/pemesanantiketkereta/Booking.java
@@ -1,18 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pemesanantiketkereta;
 
-/**
- *
- * @author AdityaEM
- */
 public class Booking extends Tiket{
     private int kodeBooking;
 
-    public Booking(String jamBerangkat, String tglBerangkat, String tujuan, String asal) {
-        super(jamBerangkat, tglBerangkat, tujuan, asal);
+    public Booking(int kode, int id, Kereta kereta, String jamBerangkat, String jamSampai, String tglBerangkat, String asal, String tujuan, int harga) {
+        super(id, kereta, jamBerangkat, jamSampai, tglBerangkat, asal, tujuan, harga); 
+        this.kodeBooking = kode;
     }
+ 
+    public void showBooking(){
+        System.out.println("Kode booking : " + this.kodeBooking + " ,harap checkin di stasiun terdekat");
+    }
+
+    public int getKodeBooking() {
+        return kodeBooking;
+    }
+
+    public void setKodeBooking(int kodeBooking) {
+        this.kodeBooking = kodeBooking;
+    }
+    
+    
 }
